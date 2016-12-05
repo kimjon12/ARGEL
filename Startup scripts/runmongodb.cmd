e437425ce44dd08a65401c8f4109ada0ea3c213b
David Kim <david.j.kim@hotmail.com>
2016-12-05 16:44:49 -0500

    updated argeldb path for server

--- Startup scripts/runmongodb.cmd
+++ Startup scripts/runmongodb.cmd
@@ -1,3 +1,3 @@
 ECHO HELLO
 cd "C:\Program Files\MongoDB\Server\3.4\bin\"
-START mongod --dbpath "C:\Users\David\Documents\GitHub\argeldb"
+START mongod --dbpath "C:\Program Files\MongoDB\Server\3.4\argeldb"
